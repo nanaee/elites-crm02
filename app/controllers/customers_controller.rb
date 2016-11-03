62170c2e883d90d9b3eb5213dfed482ffb72aaaf
saitonanae <saito-nanae@av.avex.co.jp>
2016-11-03 14:57:32 +0000

    09_基本課題終了

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!
   before_action :customer_post, only: [:show, :edit, :destroy, :update]
   
   def index
